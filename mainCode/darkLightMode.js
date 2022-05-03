b5014cea77274e57e3151dcdd2bc53b6c4af4ba2
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-05-03 19:01:08 -0300

    setting images for the languages

--- mainCode/darkLightMode.js
+++ mainCode/darkLightMode.js
@@ -8,6 +8,21 @@ const titles = document.querySelectorAll(".dark-titles");
 const text = document.querySelectorAll(".dark-text");
 const backG = document.querySelector(".darkBackG");
 const menuBtn = document.querySelector(".darkMenu");
+const htmlImg = document.querySelector(".htmlImgDark");
+const cssImg = document.querySelector(".cssImgDark");
+const jsImg = document.querySelector(".jsImgDark");
+
+function setDarkImg() {
+    htmlImg.classList.replace("htmlImgLight","htmlImgDark");
+    cssImg.classList.replace("cssImgLight","cssImgDark");
+    jsImg.classList.replace("jsImgLight","jsImgDark");
+}
+
+function setLightImg() {
+    htmlImg.classList.replace("htmlImgDark","htmlImgLight");
+    cssImg.classList.replace("cssImgDark","cssImgLight");
+    jsImg.classList.replace("jsImgDark","jsImgLight");
+}
 
 function textAndTitlesDark() {
     for(let i = 0; i < text.length; i++) {
@@ -37,7 +52,8 @@ button.addEventListener('click',()=>{
 	textAndTitlesDark(),
 	button.title = "Light Mode",
 	sun.classList.remove("hide"),
-	moon.classList.add("hide")
+	moon.classList.add("hide"),
+	setDarkImg()
     )
     : (body.classList.replace("darkMode","lightMode"),
 	menuBtn.classList.replace("darkMenu","lightMenu"),
@@ -45,6 +61,7 @@ button.addEventListener('click',()=>{
 	textAndTitlesLight(),
 	button.title = "Dark Mode",
 	moon.classList.remove("hide"),
-	sun.classList.add("hide")
+	sun.classList.add("hide"),
+	setLightImg()
 	);
 })
